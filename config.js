2ceb800c1cc53d160c1d6d6eae8a51aa5eff71cb
ToanGiNO <101492544+ToanGiNO@users.noreply.github.com>
2022-03-13 13:18:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,5 +16,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Anh Yêu Bé nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, anh qua đón bé đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.messenger.com/t/100004558868907/'
+    messLink: 'https://www.facebook.com/profile.php?id=100077905759988'
 }
